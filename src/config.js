b83589b7ce74d7d96bd5eb5fbc123b04762cd42e
gusiri <dreamerty@postech.ac.kr>
2019-09-13 12:17:48 +0900

    contract addresses added. faster block refresh time.

--- src/config.js
+++ src/config.js
@@ -1,46 +1,104 @@
-export const API_URL = process.env.NODE_ENV === 'production' ?
-  'https://api.etherview.net/blocks' :
-  'http://localhost:8000/blocks';
-export const REFRESH_TIME = 1000 * 10;
+export const API_URL = 'http://141.223.82.133:8000/blocks';
+export const REFRESH_TIME = 100 * 10;
 export const TX_BLACK_ETHER = 20;
 export const CHAIN_MARGIN_TOP = 20;
-export const ETHERSCAN_BASE_URL = 'https://etherscan.io';
+export const ETHERSCAN_BASE_URL = 'https:ropsten.etherscan.io';
 export const MENU_OPTIONS = {
-  contractCalls: false,
-  contractCreates: false,
+  contractCalls: true,
+  stakeManager: false,
+  splytManager: false,
+  assetManager: false,
+  orderManager: false,
+  arbitrationManger: false,
+  reputationManger: false,
   sortOrder: 'index',
   fromAddress: '',
   fromColor: '',
   toAddress: '',
   toColor: ''
 };
+
+export const STAKE_MANAGER = '0x7b11f2dd3552fb6513e24826a5b34eb6465787c7';
+export const SPLYT_MANAGER = '0xc53b41e624a17fb1f19f621db231a538043e2a11';
+export const ASSET_MANAGER = '0x9ecf2ec68b477594670ea9fb93e7bbaae1cf0bde';
+export const ORDER_MANAGER = '0xf4e93a1580964eddf64fed1c7f2f1f2f06d61147';
+export const ARBITRATION_MANAGER = '0x1d8f4d6a5a74d8319402f3e63f8568aa9297c0ab';
+export const REPUTATION_MANAGER = '0x7c1dad843f93991bc6638c647d53eef0727e0b7c';
+
+
+
 export const FROM_DEFAULT_COLOR = '#FFF491';
 export const TO_DEFAULT_COLOR = '#46d3f5';
-export const FROM_EXAMPLES = [];
-export const TO_EXAMPLES = [
+export const FROM_EXAMPLES = [
+
   {
-    name: 'EOS - EOS.IO',
-    address: '0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0',
-    color: '#7903f7'
+    name: 'Stake Manager',
+    address: '0x7b11f2dd3552fb6513e24826a5b34eb6465787c7',
+    color: '#3575dd'
   },
   {
-    name: 'MANA - Decentraland',
-    address: '0x0f5d2fb29fb7d3cfee444a200298f468908cc942',
+    name: 'Splyt Manager',
+    address: '0xc53b41e624a17fb1f19f621db231a538043e2a11',
     color: '#E68346'
   },
   {
-    name: 'SUB - Substratum',
-    address: '0x12480e24eb5bec1a9d4369cab6a80cad3c0a377a',
+    name: 'Asset Manager',
+    address: '0x9ecf2ec68b477594670ea9fb93e7bbaae1cf0bde',
     color: '#D4463C'
   },
   {
-    name: 'ZRX - 0xProject',
-    address: '0xe41d2489571d322189246dafa5ebde1f4699f498',
+    name: 'Order Manager',
+    address: '0xf4e93a1580964eddf64fed1c7f2f1f2f06d61147',
     color: '#a7fb40'
   },
   {
-    name: 'CryptoKittiesCore',
-    address: '0x06012c8cf97bead5deae237070f9587f8e7a266d',
+    name: 'Arbitration Manager',
+    address: '0x1d8f4d6a5a74d8319402f3e63f8568aa9297c0ab',
     color: '#fbe734'
+  },
+  {
+    name: 'Reputation Manager',
+    address: '0x7c1dad843f93991bc6638c647d53eef0727e0b7c',
+    color: '#7903f7'
   }
+
+
+];
+export const TO_EXAMPLES = [
+
+
+  {
+    name: 'Stake Manager',
+    address: '0x7b11f2dd3552fb6513e24826a5b34eb6465787c7',
+    color: '#3575dd'
+  },
+  {
+    name: 'Splyt Manager',
+    address: '0xc53b41e624a17fb1f19f621db231a538043e2a11',
+    color: '#E68346'
+  },
+  {
+    name: 'Asset Manager',
+    address: '0x9ecf2ec68b477594670ea9fb93e7bbaae1cf0bde',
+    color: '#D4463C'
+  },
+  {
+    name: 'Order Manager',
+    address: '0xf4e93a1580964eddf64fed1c7f2f1f2f06d61147',
+    color: '#a7fb40'
+  },
+  {
+    name: 'Arbitration Manager',
+    address: '0x1d8f4d6a5a74d8319402f3e63f8568aa9297c0ab',
+    color: '#fbe734'
+  },
+  {
+    name: 'Reputation Manager',
+    address: '0x7c1dad843f93991bc6638c647d53eef0727e0b7c',
+    color: '#7903f7'
+  }
+
+
+
+
 ];
